a44bb3d01961365049cfe6be0c7af18b7458f319
ramironinni <ramironinni@gmail.com>
2021-09-25 19:29:35 -0300

    Create filter in Search. Create dummyDb to avoid api fetch issues

--- conservatorio/src/components/Search/Search.js
+++ conservatorio/src/components/Search/Search.js
@@ -1,17 +1,36 @@
 import SearchBar from './SearchBar/SearchBar';
 import useFetch from '../../utils/useFetch';
 import UserCard from './UserCard/UserCard';
+import { useState } from 'react';
+import { dummyDB } from '../../utils/dummy-db';
 
 const Search = () => {
     const { data, isPending, error } = useFetch(
-        'https://randomuser.me/api/?page=1&results=4&seed=abc',
-        4,
+        'https://randomuser.me/api/?page=1&results=6&seed=abc',
+        6,
         1
     );
 
+    const [query, setQuery] = useState('');
+    const queryChangeHandler = (query) => {
+        setQuery(query);
+    };
+
+    const getFilterdUsers = () => {
+        const filteredUsers = dummyDB.results.filter((user) => {
+            const firstNameFound = user.name.first.includes(query);
+            const lastNameFound = user.name.last.includes(query);
+            const cityFound = user.location.city.includes(query);
+            return firstNameFound || lastNameFound || cityFound;
+        });
+        return filteredUsers;
+    };
+
+    const filteredUsers = getFilterdUsers();
+
     return (
         <div className="container search-container">
-            <SearchBar />
+            <SearchBar query={query} onQueryChange={queryChangeHandler} />
             <div className="results-container">
                 {isPending && (
                     <div className="results-pending">Loading ...</div>
@@ -19,21 +38,21 @@ const Search = () => {
                 {error && (
                     <div className="results-pending">Cannot fetch the data</div>
                 )}
-                {data && (
-                    <div className="row results-list">
-                        {console.log(data.results)}
-                        {data.results.map((user, i) => {
-                            return (
-                                <UserCard
-                                    name={user.name.first}
-                                    last={user.name.last}
-                                    img={user.picture.thumbnail}
-                                    key={i}
-                                />
-                            );
-                        })}
-                    </div>
-                )}
+
+                {/* {data && ( */}
+                <div className="row results-list">
+                    {filteredUsers.map((user, i) => {
+                        return (
+                            <UserCard
+                                name={user.name.first}
+                                last={user.name.last}
+                                img={user.picture.thumbnail}
+                                key={i}
+                            />
+                        );
+                    })}
+                </div>
+                {/* )} */}
             </div>
         </div>
     );
--- conservatorio/src/components/Search/SearchBar/SearchBar.js
+++ conservatorio/src/components/Search/SearchBar/SearchBar.js
@@ -1,11 +1,22 @@
-const SearchBar = () => {
+const SearchBar = ({ query, onQueryChange }) => {
+    const searchSubmitHandler = (e) => {
+        e.preventDefault();
+    };
+
+    const queryChangeHandler = (e) => {
+        onQueryChange(e.target.value);
+    };
+
     return (
-        <form className="d-flex m-5">
+        <form className="d-flex m-5" onSubmit={searchSubmitHandler}>
             <input
                 className="form-control me-2"
                 type="search"
+                name="query"
                 placeholder="name"
                 aria-label="Search"
+                value={query}
+                onChange={queryChangeHandler}
             />
             <button className="btn btn-primary" type="submit">
                 <i className="bi bi-search"></i>
